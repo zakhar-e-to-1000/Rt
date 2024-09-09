e336ca695d19ffc738ee68fcc33e24c12b18680d
Zakhar <super.korustyvach@gmail.com>
2024-09-09 18:42:07 +0300

    All tasks done

---
+++ task2.py
@@ -0,0 +1,13 @@
+music = 'Zhadan i Sobaky'
+print(music)
+
+score = 92
+other_score = 60
+print(f'{score>other_score = }')
+print(f'{score<other_score = }')
+print(f'{score==other_score = }')
+
+is_even = True
+is_odd = False
+print(f'{is_even and is_odd = }')
+print(f'{is_even or is_odd = }')
---
+++ task3.py
@@ -0,0 +1,6 @@
+from math import cos, log
+x=1.155
+y=3.981
+result = log(x**2 + 4*x*y + y**2) \
+         - 12*cos(x*y**4) + 13*x**6
+print(result)
