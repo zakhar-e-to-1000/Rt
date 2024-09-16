e2b76133af6adebcc2d02d2dae3711a6e987a023
Zakhar <super.korustyvach@gmail.com>
2024-09-16 16:50:23 +0300

    Little addition

--- task2.py
+++ task2.py
@@ -1,11 +1,25 @@
 from math import cos
-def get_table(f):
+#general method
+def get_table_1(f):
     scale = 10
     table = dict()
     for num in range(1, 11, 1):
         x = num/scale
         table[x] = f(x)
     return table
+
+#going more optimized using task indo
+def get_table_2():
+    #use the biggest value to ensure
+    # that error won't be larger than has to for smaller cases
+    scale = 10
+    table = dict()
+    computed = fsum_x(1)
+    for num in range(1, 11, 1):
+        x = num/scale
+        table[x] = x*computed
+    return table
+
 
 def fsum_x(x):
     df = 0.001
@@ -18,5 +32,6 @@ def fsum_x(x):
         k += 1
     return result
 
-table = get_table(fsum_x)
+table = get_table_1(fsum_x)
 print(table)
+
